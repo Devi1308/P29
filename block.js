d6a5bfcc3af0b9dee734d27b6d9a4481f22b44b8
Devi1308 <76687597+Devi1308@users.noreply.github.com>
2021-04-19 10:55:42 +0530

    Add files via upload

---
+++ block.js
@@ -0,0 +1,22 @@
+class Block{
+    constructor(x, y, width, height) {
+        var options = {
+                     
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+        this.image= loadImage("blockImg.png");
+      }
+      display(){
+        var angle = this.body.angle;
+        var pos= this.body.position;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image,0,0,60,45 );
+        pop();
+      }
+}
---
+++ block2.js
@@ -0,0 +1,22 @@
+class Block2{
+    constructor(x, y, width, height) {
+        var options = {
+                     
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+        this.image= loadImage("block2Img.png");
+      }
+      display(){
+        var angle = this.body.angle;
+        var pos= this.body.position;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image,0,0,45,45 );
+        pop();
+      }
+}
